73f80452dc44cc793e50fc55b73bfcfb3eba332b
masonscarvie <masonscarvie@gmail.com>
2026-04-23 15:50:29 -0700

    Small fix to the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
                 <div class="leftalign">
                     <div class="navlogo">✿</div>
                     <div class="navlinks"><a href="/">Volunteer Management System</a></div>
-                    <div class="navlinks"><a href="/events">About</a></div>
-                    <div class="navlinks"><a href="/login">Events</a></div>
-                    <div class="navlinks"><a href="/signup">Partnerships</a></div>
+                    <div class="navlinks"><a href="./../pages/about">About</a></div>
+                    <div class="navlinks"><a href="./../pages/events">Events</a></div>
+                    <div class="navlinks"><a href="./../pages/partnerships">Partnerships</a></div>
                 </div>
                 <div class="rightalign">
-                    <div class="navlinksusername">USERNAME</div>
-                    <a href="/login"><div class="navbutton">Log Out</div></a>
+                    <div class="navlinksusername"><a href="./../pages/account-management">USERNAME</a></div>
+                    <a href="./../pages/logout"><div class="navbutton">Log Out</div></a>
                 </div>
             </div>
         </nav>
@@ -30,8 +30,8 @@ function Navbar() {
                     <div class="navlinks"><a href="/signup">Partnerships</a></div>
                 </div>
                 <div class="rightalign">
-                    <div class="navlinksusername">Log In</div>
-                    <a href="/login"><div class="navbutton">Sign Up</div></a>
+                    <div class="navlinksusername"><a href="./../pages/login">Log In</a></div>
+                    <a href="./../pages/signup"><div class="navbutton">Sign Up</div></a>
                 </div>
             </div>
         </nav>
